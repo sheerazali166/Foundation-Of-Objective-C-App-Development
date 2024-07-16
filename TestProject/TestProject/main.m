3f04b0d317ca820236c70b448e26b5d6ec9c5bed
Software Scientist <sheerazali656@gmail.com>
2024-07-16 15:14:34 +0500

    initial commit

---
+++ TestProject/TestProject/main.m
@@ -0,0 +1,43 @@
+//
+//  main.m
+//  TestProject
+//
+//  Created by Sheeraz on 7/16/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+ 
+    int A = 1;
+    int B = 0;
+    
+    int C = 0;
+    int D = 0;
+    
+    // insert code here...
+    NSLog(@"Hello, Kinza");
+    
+    if (A == B) {
+        
+        printf("A equals B\n");
+        
+    } else {
+        
+        printf("A does not equals B\n");
+        
+    }
+    
+    if (C == D) {
+        
+        printf("A equals B\n");
+        
+    } else {
+        
+        printf("A does not equals B\n");
+        
+    }
+    
+
+    return 0;
+}
